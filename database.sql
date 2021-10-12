be19d3056e5cb7e6c667f0d7b029aecdcaf10a3a
Russell Barr <rbarrsquared@gmail.com>
2021-10-12 16:49:37 -0500

    updated db.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,17 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- Database named "solar_sleep_app"
+
+-- Table to store user data
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR (80) NOT NULL,
+    "last_name" VARCHAR (80) NOT NULL,
+    "email" VARCHAR (80) NOT NULL,
+    "date_of_birth" DATE NOT NULL
 );
+
+
