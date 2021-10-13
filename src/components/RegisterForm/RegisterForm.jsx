6062d6a7c9e3311b24de0004df6593fa32999ecd
Russell Barr <rbarrsquared@gmail.com>
2021-10-12 20:53:45 -0500

    updated registerform for new user data inputs

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [doBirth, setdoBirth] = useState(0);
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -15,6 +19,10 @@ function RegisterForm() {
       payload: {
         username: username,
         password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        doBirth, doBirth,
       },
     });
   }; // end registerUser
@@ -48,6 +56,54 @@ function RegisterForm() {
             value={password}
             required
             onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="first_name">
+          First Name:
+          <input
+            type="text"
+            name="first_name"
+            value={firstName}
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="last_name">
+          Last Name:
+          <input
+            type="text"
+            name="last_name"
+            value={lastName}
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email Address:
+          <input
+            type="text"
+            name="last_name"
+            value={email}
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="doBirth">
+          Date of Birth:
+          <input
+            type="date"
+            name="doBirth"
+            value={doBirth}
+            required
+            onChange={(event) => setdoBirth(event.target.value)}
           />
         </label>
       </div>
