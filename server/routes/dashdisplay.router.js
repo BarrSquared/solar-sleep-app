64ec8ac8c710b12dd08249edff6e75ed5f622760
Russell Barr <rbarrsquared@gmail.com>
2021-10-15 12:02:47 -0500

    get pulling data from server, bug triggering infinity loop for GET

--- server/routes/dashdisplay.router.js
+++ server/routes/dashdisplay.router.js
@@ -9,7 +9,18 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
   console.log('In dashdisplay router GET.');
-  console.log('In display GET, req.user: ', req.user);
+  console.log('In display GET, req.user: ', req.user.id);
+  const userId = req.user.id;
+  const queryText = `
+    SELECT * FROM "exposuredata" WHERE "user_id" = $1
+    ORDER BY "date" DESC;
+  `;
+  pool.query(queryText, [userId])
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+        console.log('Error making SELECT for user dashboard table: ', error);
+        res.sendStatus(500);
+    })
 });
 
 /**
