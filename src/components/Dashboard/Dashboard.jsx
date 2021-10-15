47a1b3c4d8ec61fd8346900a7f53e1c0d7b41af5
Russell Barr <rbarrsquared@gmail.com>
2021-10-15 11:08:30 -0500

    dashboard set to receive GET data, now to set reducer and look at routes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Dashboard() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const dashData = useSelector((store) => store.dashdisplayReducer);
+
+  useEffect(() => {
+    dispatch({ type: 'SET_DASHBOARD_TABLE'});
+  }, [dispatch]);
+
   return (
     <div className="container">
       <h3>Welcome, {user.username}!</h3>
       <p>Your ID is: {user.id}</p>
+      <p> {JSON.stringify(dashData)} </p>
       <LogOutButton className="btn" />
     </div>
   );
