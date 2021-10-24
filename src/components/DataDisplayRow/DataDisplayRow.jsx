ae0cb5fe7fa65ff78d4a206df6fb633b28b4e291
Russell Barr <rbarrsquared@gmail.com>
2021-10-23 23:38:35 -0500

    mui updated, fixed bug

--- src/components/DataDisplayRow/DataDisplayRow.jsx
+++ src/components/DataDisplayRow/DataDisplayRow.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function DataDisplayTable({ log, handleEditClick, handleDeleteClick }) {
     const dispatch = useDispatch();
