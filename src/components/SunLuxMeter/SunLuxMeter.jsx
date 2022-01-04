1ef1f75ce576ff0dbcdbfba0539667558496c33e
Russell Barr <rbarrsquared@gmail.com>
2022-01-04 13:42:38 -0600

    start of feature to capture sun lux using ambient sensor

---
+++ src/components/SunLuxMeter/SunLuxMeter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { Table, TableBody, TableCell, TableContainer, Typography, TableHead, TableRow, Paper, Button, ButtonGroup } from '@mui/material';
+
+function SunLuxMeter() {
+//Intent of this feature is to have a separate page at first
+//to use the ambient sensor to caputre lux data,
+//then integrate this feature as a button on the form to capture
+//beginning and ending exposure
+
+    return (
+<div className="container">
+    
+</div>
+    );
+}
+
+export default SunLuxMeter;
